b554a0c31673844e212908ea5b32ebdeb6ef7316
php freak <php.freak6@gmail.com>
2021-04-09 17:18:12 +0530

    Checking git push

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\StudentLoginController;
 use App\Http\Controllers\CreatePinController;
 
-
 /* STUDENT ROUTES */
 
 Auth::routes(['Login' => false, 'register' => false]);
@@ -17,15 +16,15 @@ Route::post('findParentMobile', [StudentLoginController::class, 'findParentMobil
 Route::get('dashboard', [UserController::class, 'dashboard']);
 
 Route::group(['middleware' => ['student']], function () {
-	Route::get('/signin-signout-student', [StudentLoginController::class, 'studentLogin'])->name('loginWithPin');
-	Route::post('student-login', [StudentLoginController::class, 'login'])->name('slogin');
-	Route::get('create-pin', [CreatePinController::class, 'createPin'])->name('createPin');
-	Route::post('send-otp', [CreatePinController::class, 'sendOtp'])->name('send_otp');
-	Route::get('confirm-otp', [CreatePinController::class, 'confirmOtp'])->name('confirmOtp');
-	Route::post('confirm-otp/{token}', [CreatePinController::class, 'matchOtp'])->name('matchOtp');
-	Route::get('reset-pin/{token}', [CreatePinController::class, 'resetPinForm'])->name('resetPin');
-	Route::post('reset-pin/{token}', [CreatePinController::class, 'resetPin'])->name('resetPinPost');
-	Route::get('forgot-pin', [CreatePinController::class, 'createPin'])->name('forgotPin');
+    Route::get('/signin-signout-student', [StudentLoginController::class, 'studentLogin'])->name('loginWithPin');
+    Route::post('student-login', [StudentLoginController::class, 'login'])->name('slogin');
+    Route::get('create-pin', [CreatePinController::class, 'createPin'])->name('createPin');
+    Route::post('send-otp', [CreatePinController::class, 'sendOtp'])->name('send_otp');
+    Route::get('confirm-otp', [CreatePinController::class, 'confirmOtp'])->name('confirmOtp');
+    Route::post('confirm-otp/{token}', [CreatePinController::class, 'matchOtp'])->name('matchOtp');
+    Route::get('reset-pin/{token}', [CreatePinController::class, 'resetPinForm'])->name('resetPin');
+    Route::post('reset-pin/{token}', [CreatePinController::class, 'resetPin'])->name('resetPinPost');
+    Route::get('forgot-pin', [CreatePinController::class, 'createPin'])->name('forgotPin');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
